660ce61eab9e5aafac66c455ebf6daef332645f6
Estiventh Neira <estiventhneira@gmail.com>
2020-09-18 02:59:45 -0500

    mas loaders

--- src/components/Opportunities.jsx
+++ src/components/Opportunities.jsx
@@ -1,22 +1,11 @@
 import React from "react";
+import Loader from "../static/tenor.gif";
 
 function Opportunities({ Oportunidades }) {
-  /*   const [Oportunidades, setOportunidades] = useState({
-    data: 0,
-  }); */
-
-  /* useEffect(() => {
-    fetch(
-      `https://api-staging.aiesec.org/v2/opportunities?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c&only=data&page=${count}&per_page=10&api_key=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c`
-    )
-      .then((data) => data.json())
-      .then((json) => setOportunidades(json));
-  }, [count]); */
-
   return (
     <div>
-      {Oportunidades.loading ? (
-        <h1>Loading...</h1>
+      {Oportunidades.data.length <= 1 ? (
+        <img src={Loader} alt="loading app" className="loader-gif" />
       ) : (
         <div>
           <h1 className="text-lg font-bold">Oportunidades:</h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Nav from "../components/Nav";
 import Search from "../components/Search";
 import Opportunities from "../components/Opportunities";
 
+import Loader from "../static/tenor.gif";
+
 export default function Home() {
   let [count, setCount] = useState(610);
 
@@ -18,21 +20,24 @@ export default function Home() {
         ...Oportunidades,
         loading: true,
       });
+
       async function fetchData() {
-        const peticion = await fetch(
+        const request = await fetch(
           `https://api-staging.aiesec.org/v2/opportunities?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c&only=data&page=${count}&per_page=10&api_key=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c`
         );
-        const respuesta = await peticion.json();
-        console.log(Oportunidades);
-        let nuevoArray = Oportunidades.data.concat(respuesta.data);
+
+        const response = await request.json();
+        let newOpps = Oportunidades.data.concat(response.data);
         setOportunidades({
-          data: nuevoArray,
+          loading: false,
+          data: newOpps,
         });
       }
       fetchData();
     } catch (error) {
       console.log(error);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [count]);
 
   const handleClick = () => {
@@ -50,6 +55,7 @@ export default function Home() {
             Oportunidades={Oportunidades}
           ></Opportunities>
         </div>
+        <CargaSecundaria loading={Oportunidades.loading} />
         <div className="justify-center items-center flex">
           <button
             onClick={handleClick}
@@ -62,3 +68,15 @@ export default function Home() {
     </div>
   );
 }
+
+const CargaSecundaria = ({ loading }) => {
+  return (
+    <div>
+      {loading ? (
+        <img src={Loader} alt="loading app" className="loader-gif" />
+      ) : (
+        <span></span>
+      )}
+    </div>
+  );
+};
