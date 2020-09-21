4b6740b3f944a1dd027c1f45ed65e572e93fad6c
Estiventh Neira <estiventhneira@gmail.com>
2020-09-20 21:39:37 -0500

    add build

---
+++ build/precache-manifest.0a43a859e9745b8b190143d3d3c3122f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7e1143c3b1d98d9720c41dafddb7f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc3bda829035921f0330",
+    "url": "/static/css/main.89893857.chunk.css"
+  },
+  {
+    "revision": "26af5fb4e723dd56bf42",
+    "url": "/static/js/2.07dd3d8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.07dd3d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc3bda829035921f0330",
+    "url": "/static/js/main.cf704de0.chunk.js"
+  },
+  {
+    "revision": "85bd3631f10000781693",
+    "url": "/static/js/runtime-main.5afd010c.js"
+  },
+  {
+    "revision": "b213dc57bfa0ff88ca7522139c937acf",
+    "url": "/static/media/tenor.b213dc57.gif"
+  }
+]);
